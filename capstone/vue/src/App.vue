063affddf72638a5c4605b69736e5c100353f44a
Andrew Canter <acanterohio@gmail.com>
2022-12-07 12:27:06 -0500

    Added body and router

--- capstone/vue/src/App.vue
+++ capstone/vue/src/App.vue
@@ -1,10 +1,20 @@
 <template>
+<body>
   <div id="app">
     <div id="nav">
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'application' }">Application</router-link>&nbsp;|&nbsp;
+      <router-link v-bind:to="{ name: 'login' }">Login</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
     </div>
     <router-view />
   </div>
+</body>
+  
 </template>
+
+<style>
+ body {
+   background-color: gray;
+ }
+</style>
